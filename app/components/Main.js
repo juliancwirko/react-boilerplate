e920ce2a14abdceab25342a7bd946427f8d6368b
Julian Ćwirko <julian.cwirko@gmail.com>
2015-08-08 17:26:01 +0200

    router state demo

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,14 +1,37 @@
 import React from 'react';
-
+import _ from 'lodash';
 import {Router, RouteHandler} from 'react-router';
 
 class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isHome: true
+        }
+    }
+    init() {
+        this.setState({
+            isHome: _.isEmpty(this.router.getCurrentParams())
+        });
+    }
+    componentWillMount() {
+        this.router = this.context.router;
+    }
+    componentDidMount() {
+        this.init();
+    }
+    componentWillReceiveProps() {
+        this.init();
+    }
     render() {
         return (
             <div>
                 <header className="header">
                     <div className="container">
-                        Demo App
+                        <span>Demo App: </span>
+                        {this.state.isHome ? <span>
+                            This is homepage
+                        </span> : <span>This is subpage</span>}
                     </div>
                 </header>
                 <section>
@@ -20,5 +43,9 @@ class Main extends React.Component {
         )
     }
 }
+
+Main.contextTypes = {
+    router: React.PropTypes.func.isRequired
+};
 
 export default Main;
