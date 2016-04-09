d79d7cdc52804c0121ca0fc3f0bdef71120ff8d5
evenstensberg <dweaster@live.no>
2016-04-09 19:18:16 +0200

    Added some snacks

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+var compiler = webpack(config);
 
-new WebpackDevServer(webpack(config), {
+new WebpackDevServer(compiler, {
     contentBase: './public',
     publicPath: config.output.publicPath,
     hot: true,
---
+++ tests.node.js
@@ -0,0 +1,1 @@
+import test from './__tests__/Spinner-test'
